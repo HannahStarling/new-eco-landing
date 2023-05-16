2b6f2dd25c12fbfdcba4c49193acbb7619dfe3c5
alekseeva_ekaterina <kat_9898@mail.ru>
2023-05-16 12:06:54 +0200

    TMS-7166 Add input and styles

---
+++ src/ui-elements/ui-input/ui-input.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import classNames from 'classnames';
+import './ui-input.scss';
+
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+  classname?: string;
+  label?: string;
+};
+
+export const UiInput: FC<Props> = ({ classname, required, id, label, ...props }) => {
+  return (
+    <div className="ui-input">
+      <label className="ui-input__label" htmlFor={id}>
+        {label}
+        {required && <span className="ui-input__label-required">*</span>}
+      </label>
+      <input
+        id={id}
+        type="search"
+        required={required}
+        className={classNames(classname, 'ui-input__field')}
+        {...props}
+      />
+    </div>
+  );
+};
