93fb900463aad4bae294d19e8aa5a42f30f56e86
Anna Tuban <mshannahstarling@gmail.com>
2023-05-16 14:28:52 +0200

    fix li/paragraph

--- src/ui-elements/ui-list-item/ui-list-item.tsx
+++ src/ui-elements/ui-list-item/ui-list-item.tsx
@@ -2,8 +2,8 @@ import classNames from "classnames";
 
 export default function UiListItem({children, className, ...props}) {
   return (
-    <ul className={classNames(className, `list`)} {...props}>
+    <li className={classNames(className, `list`)} {...props}>
       {children}
-    </ul>
+    </li>
   );
 };
--- src/ui-elements/ui-paragraph/ui-paragraph.tsx
+++ src/ui-elements/ui-paragraph/ui-paragraph.tsx
@@ -2,12 +2,15 @@ import './ui-paragraph.scss'
 import classNames from "classnames";
 
 export enum ParagraphSize {
+  smallBold = 'paragraph_small-bold',
+  mediumBold = 'paragraph_medium-bold',
+  largeBold = 'paragraph_large-bold',
   small = 'paragraph_small',
-  general = 'paragraph_general',
+  medium = 'paragraph_medium',
   large = 'paragraph_large',
 }
 
-export default function UiParagraph({children, className, size = ParagraphSize.general, ...props}: {
+export default function UiParagraph({children, className, size = ParagraphSize.medium, ...props}: {
   children: React.ReactNode;
   size: ParagraphSize
 }) {
