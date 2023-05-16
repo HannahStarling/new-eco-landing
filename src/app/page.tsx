7759a0ca0d1dca2a57bb8a2836dca669ac130f5e
Anna Tuban <mshannahstarling@gmail.com>
2023-05-16 11:52:05 +0200

    fix naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import {HeaderComponent} from "@/components/header-component/header-component";
 import {FooterComponent} from "@/components/footer-component/footer-component";
-import {MainComponent} from "@/components/main-component.tsx/main-component";
+import {MainComponent} from "@/components/main-component/main-component";
 import "./styles/page.scss";
 import Title from "@/ui-elements/title/title";
 
