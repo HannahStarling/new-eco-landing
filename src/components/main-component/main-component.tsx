6768cd7a4926db86fd84792f82d0330a600146e1
vmakarov <vbmakarov.info@gmail.com>
2023-05-17 15:05:10 +0300

    fixed main container

--- src/components/main-component/main-component.tsx
+++ src/components/main-component/main-component.tsx
@@ -2,5 +2,9 @@ import "./styles.scss";
 import { FC } from "react";
 
 export const MainComponent: FC = ({ children }) => {
-  return <main className="main main__container">{children}</main>;
+  return (
+    <main className="main">
+      <div className="main__container">{children}</div>
+    </main>
+  );
 };
