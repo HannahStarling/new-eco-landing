1d9b7029efbd84a2f6edf6461d5c883ebcf5ffdf
vmakarov <vbmakarov.info@gmail.com>
2023-05-17 11:56:15 +0300

    header sticky

--- src/components/header-component/header-component.tsx
+++ src/components/header-component/header-component.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import "./styles.scss";
-import {HeaderLogoComponent} from "./header-logo-component/header-logo-component";
-import {HeaderButtonComponent} from "./header-button-component/header-button-component";
-import {MenuComponent} from "@/components/menu-component/menu-component";
+import { HeaderLogoComponent } from "./header-logo-component/header-logo-component";
+import { HeaderButtonComponent } from "./header-button-component/header-button-component";
+import { MenuComponent } from "@/components/menu-component/menu-component";
+import { useRef } from "react";
+import { useHeaderScroll } from "@/hooks/useHeaderScroll";
+import classNames from "classnames";
 
 export const HeaderComponent = () => {
+  const headerRef = useRef<HTMLElement>(null);
+  const isScroll = useHeaderScroll({ headerRef });
+
   return (
-    <header className="header">
-      <HeaderLogoComponent/>
-      <MenuComponent/>
-      <HeaderButtonComponent/>
+    <header
+      ref={headerRef}
+      className={classNames("header", isScroll ? "header_sticky" : null)}
+    >
+      <HeaderLogoComponent />
+      <MenuComponent />
+      <HeaderButtonComponent />
     </header>
   );
 };
