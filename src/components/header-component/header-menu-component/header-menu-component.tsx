e8c6bf49b67799c3966df51636ca1912aba6bded
vmakarov <vbmakarov.info@gmail.com>
2023-05-17 15:33:55 +0300

    add breakpoints scss

--- src/components/header-component/header-menu-component/header-menu-component.tsx
+++
@@ -1,9 +0,0 @@
-import {MenuComponent} from "@/components/menu-component/menu-component";
-
-export const HeaderMenuComponent = () => {
-  return (
-    <nav className="header__menu menu">
-      <MenuComponent/>
-    </nav>
-  );
-};
