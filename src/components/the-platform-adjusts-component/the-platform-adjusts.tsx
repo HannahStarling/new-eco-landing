118224f3c18059cc44d22cf447deb7b81f1a4fd0
Anna Tuban <mshannahstarling@gmail.com>
2023-05-17 18:06:20 +0200

    add adaptive for the-platform-adjusts

--- src/components/the-platform-adjusts-component/the-platform-adjusts.tsx
+++ src/components/the-platform-adjusts-component/the-platform-adjusts.tsx
@@ -3,12 +3,7 @@ import { FC } from "react";
 import { UiSection } from "@/ui-elements/ui-section/ui-section";
 import { UiSectionTitle } from "@/ui-elements/ui-section-title/ui-section-title";
 import { UiAccent } from "@/ui-elements/ui-accent/ui-accent";
-import {
-  ParagraphSize,
-  UiParagraph,
-} from "@/ui-elements/ui-paragraph/ui-paragraph";
-import { UiImage } from "@/ui-elements/ui-image/ui-image";
-import { ImageName, ImageSize } from "@/ui-elements/ui-image/image-names";
+import { ThePlatformAdjustsContent } from "@/components/the-platform-adjusts-component/the-platform-adjusts-content";
 
 interface IProps {}
 
@@ -17,26 +12,6 @@ export const ThePlatformAdjusts: FC<IProps> = () => (
     <UiSectionTitle>
       <UiAccent>Платформа подстраивается</UiAccent> под любые бизнес-процессы*
     </UiSectionTitle>
-    <UiParagraph
-      className={"the-platform-adjusts__sub"}
-      size={ParagraphSize.extraLarge}
-    >
-      Система рассчитывает маршруты по&nbsp;13&nbsp;конфигурациям:
-      по&nbsp;7&nbsp;основным и&nbsp;6 дополнительным
-    </UiParagraph>
-    <UiImage
-      size={ImageSize[ImageName.Process]}
-      name={ImageName.Process}
-      alt={"Экран планировщика."}
-    />
-    <UiParagraph
-      className={"the-platform-adjusts__footnote"}
-      centered
-      size={ParagraphSize.extraSmall}
-    >
-      *Проанализировали более 500 логистических
-      бизнес&nbsp;-&nbsp;процессов&nbsp;и&nbsp;на основе их разработали
-      конфигурации
-    </UiParagraph>
+    <ThePlatformAdjustsContent />
   </UiSection>
 );
