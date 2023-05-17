ab5dc558a10092d6ea5929466698e4a8add2df2a
alekseeva_ekaterina <kat_9898@mail.ru>
2023-05-17 11:28:57 +0200

    TMS-7178 Add responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-import {HeaderComponent} from "@/components/header-component/header-component";
-import {FooterComponent} from "@/components/footer-component/footer-component";
-import {MainComponent} from "@/components/main-component/main-component";
-import "./styles/page.scss";
-import {Benefits} from "@/components/benefits-component/benefits";
-import {UiSection} from "@/ui-elements/ui-section/ui-section";
+import { HeaderComponent } from '@/components/header-component/header-component';
+import { FooterComponent } from '@/components/footer-component/footer-component';
+import { MainComponent } from '@/components/main-component/main-component';
+import './styles/page.scss';
+import { Benefits } from '@/components/benefits-component/benefits';
+import { UiSection } from '@/ui-elements/ui-section/ui-section';
+import { CommentsList } from '@/components/comments-list-component/comments-list';
 
 export default function Home() {
   return (
     <div className="wrapper">
-      <HeaderComponent/>
+      <HeaderComponent />
       <MainComponent>
-        <UiSection style={{paddingTop: 60, height: 554}}>Платформа Intelogis для вашего бизнеса</UiSection>
-        <Benefits/>
+        <UiSection style={{ paddingTop: 60, height: 554 }}>
+          Платформа Intelogis для вашего бизнеса
+        </UiSection>
+        <Benefits />
+        <CommentsList />
       </MainComponent>
-      <FooterComponent/>
+      <FooterComponent />
     </div>
   );
 }
