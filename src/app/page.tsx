ef2f1eb3047c9e3cdb73224fbac416488cc31c6f
Oleg Tsesko <tsesko@arview.pro>
2023-05-18 19:31:06 +0700

    remove cover duplicate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,6 @@ export default function Home() {
     <div className="wrapper">
       <HeaderComponent />
       <MainComponent>
-        <Cover />
         <Benefits />
         <PlatformModules />
         <FullCycle />
