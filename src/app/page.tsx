be0ca5201b734eb459832a6e9fc7187cf190eebc
Anna Tuban <mshannahstarling@gmail.com>
2023-05-19 09:46:02 +0200

    add doc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
-import './styles/page.scss';
-import { HeaderComponent } from '@/components/header-component/header-component';
-import { FooterComponent } from '@/components/footer-component/footer-component';
-import { MainComponent } from '@/components/main-component/main-component';
-import { Benefits } from '@/components/benefits-component/benefits';
-import { CommentsList } from '@/components/comments-list-component/comments-list';
-import { Companies } from '@/components/companies-component/companies';
-import { PlatformModules } from '@/components/platform-modules-component/platform-modules';
-import { FullCycle } from '@/components/full-cycle-component/full-cycle-component';
-import { ThePlatformAdjusts } from '@/components/the-platform-adjusts-component/the-platform-adjusts';
-import { Tender } from '@/components/tender-component/tender';
-import { Edo } from '@/components/EDO-component/EDO';
-import { MobileApp } from '@/components/mobile-app-component/mobile-app';
-import { Implementing } from '@/components/implementing-component/implementing';
-import { Tariffs } from '@/components/tariffs-component/tariffs';
-import { SchemeComponent } from '@/components/scheme-component/scheme-component';
-import { Monitoring } from '@/components/monitoring-component/monitoring-component';
-import { Planning } from '@/components/planning/planning';
+import "./styles/page.scss";
+import { HeaderComponent } from "@/components/header-component/header-component";
+import { FooterComponent } from "@/components/footer-component/footer-component";
+import { MainComponent } from "@/components/main-component/main-component";
+import { Benefits } from "@/components/benefits-component/benefits";
+import { CommentsList } from "@/components/comments-list-component/comments-list";
+import { Companies } from "@/components/companies-component/companies";
+import { PlatformModules } from "@/components/platform-modules-component/platform-modules";
+import { FullCycle } from "@/components/full-cycle-component/full-cycle-component";
+import { ThePlatformAdjusts } from "@/components/the-platform-adjusts-component/the-platform-adjusts";
+import { Tender } from "@/components/tender-component/tender";
+import { Edo } from "@/components/EDO-component/EDO";
+import { MobileApp } from "@/components/mobile-app-component/mobile-app";
+import { Implementing } from "@/components/implementing-component/implementing";
+import { Tariffs } from "@/components/tariffs-component/tariffs";
+import { SchemeComponent } from "@/components/scheme-component/scheme-component";
+import { Monitoring } from "@/components/monitoring-component/monitoring-component";
+import { Planning } from "@/components/planning/planning";
+import { IntegrationAnalysis } from "@/components/integration-analysis-component/integration-analysis";
 
 export default function Home() {
   return (
@@ -31,6 +32,7 @@ export default function Home() {
         <Tender />
         <Monitoring />
         <Edo />
+        <IntegrationAnalysis />
         <MobileApp />
         <Implementing />
         <Tariffs />
