1112eaf4aea5068efa378d1fbfa82745e0c3eff4
vmakarov <vbmakarov.info@gmail.com>
2023-05-22 14:43:14 +0300

    fixed header menu + fixed element scroll offset

--- src/components/header-component/header-button-component/header-button-component.tsx
+++ src/components/header-component/header-button-component/header-button-component.tsx
@@ -1,6 +1,8 @@
+import { TMS_URL } from "@/constants/general";
 import { BUTTONS_CONTENT } from "@/constants/texts";
 import { UiButton } from "@/ui-elements/ui-button/ui-button";
 import classNames from "classnames";
+import Link from "next/link";
 import { FC, ReactNode } from "react";
 
 interface IProps {
@@ -11,13 +13,18 @@ interface IProps {
 export const HeaderButtonComponent: FC<IProps> = ({ isWhiteHeader }) => {
   return (
     <div className="header__button button">
-      <UiButton
-        fillType="outlined"
-        mode={"none"}
-        className={classNames("button__login", isWhiteHeader ? "black" : null)}
-      >
-        {BUTTONS_CONTENT.LOGIN}
-      </UiButton>
+      <Link href={TMS_URL} rel="noopener noreferrer" target="_blank">
+        <UiButton
+          fillType="outlined"
+          mode={"none"}
+          className={classNames(
+            "button__login",
+            isWhiteHeader ? "black" : null
+          )}
+        >
+          {BUTTONS_CONTENT.LOGIN}
+        </UiButton>
+      </Link>
     </div>
   );
 };
