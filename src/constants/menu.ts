a3a49d7df6c8c3b369352cd64f937d5f984ec6d8
Anna Tuban <mshannahstarling@gmail.com>
2023-05-25 11:21:15 +0200

    fix menu order

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -12,12 +12,12 @@ export const menu = {
         sectionID: "#planning",
       },
       3: {
-        name: "Отслеживание рейсов и грузов",
-        sectionID: "#monitoring",
-      },
-      4: {
         name: "Тендерная площадка",
         sectionID: "#tender",
+      },
+      4: {
+        name: "Отслеживание рейсов и грузов",
+        sectionID: "#monitoring",
       },
       5: {
         name: "Электронные документы",
@@ -39,13 +39,13 @@ export const menu = {
     sectionID: "#implementing",
   },
   3: {
-    name: "Отзывы",
-    children: null,
-    sectionID: "#comments-list",
-  },
-  4: {
     name: "Тарифы",
     children: null,
     sectionID: "#tariffs",
+  },
+  4: {
+    name: "Отзывы",
+    children: null,
+    sectionID: "#comments-list",
   },
 };
