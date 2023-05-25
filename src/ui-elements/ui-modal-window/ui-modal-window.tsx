47eca852966bc40d9766a47108fe71a3f008f957
Anna Tuban <mshannahstarling@gmail.com>
2023-05-25 09:51:31 +0200

    add portal for modal

--- src/ui-elements/ui-modal-window/ui-modal-window.tsx
+++ src/ui-elements/ui-modal-window/ui-modal-window.tsx
@@ -1,6 +1,7 @@
 import "./ui-modal-window.scss";
 import classNames from "classnames";
 import { FC, PropsWithChildren } from "react";
+import { createPortal } from "react-dom";
 
 interface IProps {
   visible: boolean;
@@ -14,13 +15,14 @@ export const UiModal: FC<PropsWithChildren<IProps>> = ({
   visible,
   ...props
 }) => {
-  return (
+  return createPortal(
     <aside
       className={classNames("modal", className, { modal_opened: visible })}
     >
       <div {...props} className={classNames("modal__inner", className)}>
         {children}
       </div>
-    </aside>
+    </aside>,
+    document.body
   );
 };
